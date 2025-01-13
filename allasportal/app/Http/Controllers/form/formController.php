e3a659aa7670b5d4f9139f2cee5d652747cd2102
KoboldJohn <lukacs.dominik99@gmail.com>
2025-01-13 20:45:32 +0100

    controller - form

--- allasportal/app/Http/Controllers/form/formController.php
+++ allasportal/app/Http/Controllers/form/formController.php
@@ -1,11 +1,33 @@
 <?php
 
-namespace App\Http\Controllers\form;
+namespace App\Http\Controllers\Form;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\NewjobRequest;
+use App\Http\Requests\AppRequest;
+use App\Models\JobsModel;
+use App\Models\ApplicationsModel;
 
-class formController extends Controller
+class FormController extends Controller
 {
-    //
+    public function newjob()
+    {
+        return view('form.newjob');
+    }
+
+    public function store(NewjobRequest $request)
+    {
+        JobsModel::create($request->validated());
+        return view('/jobs/newjob');
+    }
+
+    public function applyStore(AppRequest $request)
+    {
+        if (ApplicationsModel::create($request->validated())) {
+        } else {
+            $j = JobsModel::all();
+            return view('job.jobs', ['records' => $j]);
+        }
+    }
 }
