82df960366c7f43f37fb8ff143284f5d9b12f237
EBTBSD <zsola.faz2002@outlook.hu>
2025-01-17 16:38:59 +0100

    felhasználó adatmódosításának Controllere
    Felhasználó adatmódosítás csontroller megírása, implementálása
    
    Co-Authored-By: KoboldJohn <163851791+KoboldJohn@users.noreply.github.com>

--- allasportal/app/Http/Controllers/auth/userController.php
+++ allasportal/app/Http/Controllers/auth/userController.php
@@ -1,11 +1,83 @@
 <?php
 
-namespace App\Http\Controllers\auth;
+namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\NevRequest;
+use App\Http\Requests\SalaryRequest;
+use App\Http\Requests\PassRequest;
+use App\Http\Requests\EmailRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
-class userController extends Controller
+class UserController extends Controller
 {
-    //
+    public function profile()
+    {
+        $user = User::all();
+        return view('auth.profile', ['records' => $user]);
+    }
+
+    public function modifyName(NevRequest $request, $id)
+    {
+        $userId = Auth::id();
+        $user = User::findOrFail($userId);
+
+        if (Auth::attempt($request->validated())) {
+            $user->name = $request->input('name');
+            $user->save();
+            return redirect()->route('auth.felhasznalo')->with("Sikeres módosítás!");
+        } else {
+            $u = User::all();
+            return view('auth.profile', ['records' => $u]);
+        }
+    }
+
+    public function modifyEmail(EmailRequest $request, $id)
+    {
+        $userId = Auth::id();
+        $user = User::findOrFail($userId);
+
+        if (Auth::attempt($request->validated()) && $request->input('oldemail') != $request->input('email') && $request->input('email') == $request->input('comfemail')) {
+            $user->email = $request->input('email');
+            $user->save();
+            return redirect()->route('auth.felhasznalo')->with("Sikeres módosítás!");
+        } else {
+            $u = User::all();
+            return view('auth.profile', ['records' => $u]);
+        }
+    }
+
+    public function modifypass(PassRequest $request, $id)
+    {
+        $userId = Auth::id();
+        $user = User::findOrFail($userId);
+
+        if (Auth::attempt($request->validated()) && $request->input('oldpass') != $request->input('password') && $request->input('password') == $request->input('comfpass')) {
+            $user->password = Hash::make($request->input('password'));
+            $user->save();
+            return redirect()->route('auth.felhasznalo')->with("Sikeres módosítás!");
+        } else {
+            $u = User::all();
+            return view('auth.profile', ['records' => $u]);
+        }
+    }
+
+    public function modifySalary(SalaryRequest $request, $id)
+    {
+        $userId = Auth::id();
+        $user = User::findOrFail($id);
+
+        if (Auth::attempt($request->validated())) {
+            $user->salary = $request->input('salary');
+            $user->sex = $request->input('sex');
+            $user->save();
+            return redirect()->route('auth.felhasznalo')->with("Sikeres módosítás!");
+        } else {
+            $u = User::all();
+            return view('auth.profile', ['records' => $u]);
+        }
+    }
 }
