c0d66e6126c0607f8bcb7d2eee9def836a44ddcf
EBTBSD <zsola.faz2002@outlook.hu>
2025-01-18 08:46:11 +0100

    Hiba javitás
    Fő Controlelrbe generálodott szöveget híbának jelezve törlésre került

--- allasportal/app/Http/Controllers/Controller.php
+++ allasportal/app/Http/Controllers/Controller.php
@@ -7,18 +7,5 @@ use Illuminate\Support\Facades\Auth;
 
 abstract class Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
-    protected function redirectToHome()
-    {
-        return redirect()->route('home');
-    }
-
-    protected function getCurrentUser()
-    {
-        return Auth::user();
-    }
 }
